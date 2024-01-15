6e641f454226a4c7e0227f3879c30e2831722337
chan9yu <wry5533@gmail.com>
2024-01-15 22:29:59 +0900

    feat(@coplore/react-ui): 컴포넌트 구성

---
+++ packages/coplore-react-ui/src/components/Flex/Flex.tsx
@@ -0,0 +1,7 @@
+import type { FlexProps } from './Flex.types';
+
+const Flex = ({ foo }: FlexProps) => {
+	return <div>{foo}</div>;
+};
+
+export default Flex;
--- packages/coplore-react-ui/src/index.ts
+++ packages/coplore-react-ui/src/index.ts
@@ -1,1 +1,7 @@
+// components
 export { default as Button } from './components/Button';
+export { default as Flex } from './components/Flex';
+
+// types
+export type * from './components/Button';
+export type * from './components/Flex';
