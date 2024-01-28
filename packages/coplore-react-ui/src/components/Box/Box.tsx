a6cbe242300ae4c08300f4eb3ebbdef3eb74eebe
chan9yu <wry5533@gmail.com>
2024-01-28 14:22:51 +0900

    feat: box 컴포넌트 구성

--- packages/coplore-react-ui/src/components/Box/Box.tsx
+++ packages/coplore-react-ui/src/components/Box/Box.tsx
@@ -1,10 +1,102 @@
-import { forwardRef } from 'react';
+import { createElement, forwardRef } from 'react';
+import classNames from 'classnames';
+
 import type { BoxProps } from './Box.types';
 
 const Box = forwardRef<HTMLElement, BoxProps>((props, ref) => {
-	const Component = 'div';
+	const {
+		as = 'div',
+		bgColor,
+		border,
+		borderBottomRadius,
+		borderColor,
+		borderLeftRadius,
+		borderRadius,
+		borderRightRadius,
+		borderTopRadius,
+		bottom,
+		boxShadow,
+		children,
+		className: classNameProp,
+		color,
+		cursor,
+		display,
+		height,
+		left,
+		margin,
+		marginBottom,
+		marginLeft,
+		marginRight,
+		marginTop,
+		maxHeight,
+		maxWidth,
+		minHeight,
+		minWidth,
+		padding,
+		paddingBottom,
+		paddingLeft,
+		paddingRight,
+		paddingTop,
+		pointerEvents,
+		position,
+		right,
+		top,
+		translateX,
+		translateY,
+		width,
+		zIndex,
+		...rest
+	} = props;
 
-	return <div>Box</div>;
+	const className = classNames(
+		'box',
+		bgColor && `box__bg-color--${bgColor.replace('.', '-')}`,
+		border && `box__border--${border}`,
+		borderBottomRadius && `box__border-bottom-radius--${borderBottomRadius}`,
+		borderColor && `box__border-color--${borderColor}`,
+		borderLeftRadius && `box__border-left-radius--${borderLeftRadius}`,
+		borderRadius && `box__border-radius--${borderRadius}`,
+		borderRightRadius && `box__border-right-radius--${borderRightRadius}`,
+		borderTopRadius && `box__border-top-radius--${borderTopRadius}`,
+		bottom && `box__bottom--${bottom}`,
+		boxShadow && `box__shadow--${boxShadow}`,
+		color && `box__color--${color.replace('.', '-')}`,
+		cursor && `box__cursor--${cursor}`,
+		display && `box__display--${display}`,
+		height && `box__height--${height}`,
+		left && `box__left--${left}`,
+		margin && `box__m--${margin}`,
+		marginBottom && `box__mb--${marginBottom}`,
+		marginLeft && `box__ml--${marginLeft}`,
+		marginRight && `box__mr--${marginRight}`,
+		marginTop && `box__mt--${marginTop}`,
+		maxHeight && `box__max-height--${maxHeight}`,
+		maxWidth && `box__max-width--${maxWidth}`,
+		minHeight && `box__min-height--${minHeight}`,
+		minWidth && `box__min-width--${minWidth}`,
+		padding && `box__p--${padding}`,
+		paddingBottom && `box__pb--${paddingBottom}`,
+		paddingLeft && `box__pl--${paddingLeft}`,
+		paddingRight && `box__pr--${paddingRight}`,
+		paddingTop && `box__pt--${paddingTop}`,
+		pointerEvents && `box__pointer-events--${pointerEvents}`,
+		position && `box__position--${position}`,
+		right && `box__right--${right}`,
+		top && `box__top--${top}`,
+		translateX && `box__translate-x--${translateX}`,
+		translateY && `box__translate-y--${translateY}`,
+		width && `box__width--${width}`,
+		zIndex && `box__z-index--${zIndex}`,
+		classNameProp && classNameProp
+	);
+
+	const elementProps = {
+		...rest,
+		ref,
+		className
+	};
+
+	return createElement(as, elementProps, children);
 });
 
 Box.displayName = 'Box';
--- packages/coplore-react-ui/src/components/Box/Box.types.ts
+++ packages/coplore-react-ui/src/components/Box/Box.types.ts
@@ -14,41 +14,231 @@ import type {
 type DefaultProps = Omit<HTMLAttributes<HTMLElement>, 'as'>;
 
 export interface BoxProps extends DefaultProps {
+	/**
+	 * - 컴포넌트의 렌더링을 지정할 React 엘리먼트 타입입니다.
+	 * @default 'div'
+	 */
 	as?: ElementType;
-	bgColor?: ColorPalette;
-	color?: ColorPalette;
-	cursor?: CoploreCursor;
-	display?: CoploreDisplay;
-	height?: CoploreSize;
-	margin?: CoploreSpacing;
-	marginBottom?: CoploreSpacing;
-	marginLeft?: CoploreSpacing;
-	marginRight?: CoploreSpacing;
-	marginTop?: CoploreSpacing;
-	maxHeight?: CoploreSize;
-	maxWidth?: CoploreSize;
-	minHeight?: CoploreSize;
-	minWidth?: CoploreSize;
-	padding?: CoploreSpacing;
-	paddingBottom?: CoploreSpacing;
-	paddingLeft?: CoploreSpacing;
-	paddingRight?: CoploreSpacing;
-	paddingTop?: CoploreSpacing;
-	pointerEvents?: CoplorePointerEvents;
-	position?: CoplorePosition;
-	width?: CoploreSize;
-	zIndex?: CoploreZIndex;
 
-	// new
-	border?: 'none' | ThemeSize;
+	/**
+	 * - 배경 색상을 설정합니다.
+	 * @default undefined
+	 */
+	bgColor?: ColorPalette;
+
+	/**
+	 * - 테두리의 크기를 설정합니다.
+	 * @default undefined
+	 */
+	border?: ThemeSize | 'none';
+
+	/**
+	 * - 하단 테두리의 반지름을 설정합니다.
+	 * @default undefined
+	 */
+	borderBottomRadius?: ThemeSize | 'none' | 'full';
+
+	/**
+	 * - 테두리 색상을 설정합니다.
+	 * @default undefined
+	 */
 	borderColor?: ColorPalette;
-	borderRadius?: 'none' | ThemeSize | 'full';
-	borderTopRadius?: 'none' | ThemeSize | 'full';
-	borderRightRadius?: 'none' | ThemeSize | 'full';
-	borderBottomRadius?: 'none' | ThemeSize | 'full';
-	borderLeftRadius?: 'none' | ThemeSize | 'full';
-	top?: CoploreSpacing | '0%' | '50%' | '100%';
-	right?: CoploreSpacing | '0%' | '50%' | '100%';
-	bottom?: CoploreSpacing | '0%' | '50%' | '100%';
-	left?: CoploreSpacing | '0%' | '50%' | '100%';
+
+	/**
+	 * - 왼쪽 테두리의 반지름을 설정합니다.
+	 * @default undefined
+	 */
+	borderLeftRadius?: ThemeSize | 'none' | 'full';
+
+	/**
+	 * - 테두리의 전체 반지름을 설정합니다.
+	 * @default undefined
+	 */
+	borderRadius?: ThemeSize | 'none' | 'full';
+
+	/**
+	 * - 오른쪽 테두리의 반지름을 설정합니다.
+	 * @default undefined
+	 */
+	borderRightRadius?: ThemeSize | 'none' | 'full';
+
+	/**
+	 * - 상단 테두리의 반지름을 설정합니다.
+	 * @default undefined
+	 */
+	borderTopRadius?: ThemeSize | 'none' | 'full';
+
+	/**
+	 * - Box 컴포넌트의 하단 위치를 설정합니다.
+	 * @default undefined
+	 */
+	bottom?: 'start' | 'center' | 'end';
+
+	/**
+	 * - Box 컴포넌트의 그림자를 설정합니다.
+	 * @default undefined
+	 */
+	boxShadow?: ThemeSize | 'none';
+
+	/**
+	 * - 텍스트 색상을 설정합니다.
+	 * @default undefined
+	 */
+	color?: ColorPalette;
+
+	/**
+	 * - 마우스 커서 스타일을 설정합니다.
+	 * @default undefined
+	 */
+	cursor?: CoploreCursor;
+
+	/**
+	 * - 컴포넌트의 표시 방법을 설정합니다.
+	 * @default undefined
+	 */
+	display?: CoploreDisplay;
+
+	/**
+	 * - 컴포넌트의 높이를 설정합니다.
+	 * @default undefined
+	 */
+	height?: CoploreSize;
+
+	/**
+	 * - Box 컴포넌트의 왼쪽 위치를 설정합니다.
+	 * @default undefined
+	 */
+	left?: 'start' | 'center' | 'end';
+
+	/**
+	 * - 외부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	margin?: CoploreSpacing;
+
+	/**
+	 * - 하단 외부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	marginBottom?: CoploreSpacing;
+
+	/**
+	 * - 왼쪽 외부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	marginLeft?: CoploreSpacing;
+
+	/**
+	 * - 오른쪽 외부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	marginRight?: CoploreSpacing;
+
+	/**
+	 * - 상단 외부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	marginTop?: CoploreSpacing;
+
+	/**
+	 * - 컴포넌트의 최대 높이를 설정합니다.
+	 * @default undefined
+	 */
+	maxHeight?: CoploreSize;
+
+	/**
+	 * - 컴포넌트의 최대 너비를 설정합니다.
+	 * @default undefined
+	 */
+	maxWidth?: CoploreSize;
+
+	/**
+	 * - 컴포넌트의 최소 높이를 설정합니다.
+	 * @default undefined
+	 */
+	minHeight?: CoploreSize;
+
+	/**
+	 * - 컴포넌트의 최소 너비를 설정합니다.
+	 * @default undefined
+	 */
+	minWidth?: CoploreSize;
+
+	/**
+	 * - 내부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	padding?: CoploreSpacing;
+
+	/**
+	 * - 하단 내부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	paddingBottom?: CoploreSpacing;
+
+	/**
+	 * - 왼쪽 내부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	paddingLeft?: CoploreSpacing;
+
+	/**
+	 * - 오른쪽 내부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	paddingRight?: CoploreSpacing;
+
+	/**
+	 * - 상단 내부 여백을 설정합니다.
+	 * @default undefined
+	 */
+	paddingTop?: CoploreSpacing;
+
+	/**
+	 * - 마우스 이벤트 허용 여부를 설정합니다.
+	 * @default undefined
+	 */
+	pointerEvents?: CoplorePointerEvents;
+
+	/**
+	 * - 컴포넌트의 위치를 설정합니다.
+	 * @default undefined
+	 */
+	position?: CoplorePosition;
+
+	/**
+	 * - Box 컴포넌트의 오른쪽 위치를 설정합니다.
+	 * @default undefined
+	 */
+	right?: 'start' | 'center' | 'end';
+
+	/**
+	 * - Box 컴포넌트의 상단 위치를 설정합니다.
+	 * @default undefined
+	 */
+	top?: 'start' | 'center' | 'end';
+
+	/**
+	 * - 가로 축으로 이동하는 정도를 설정합니다.
+	 * @default undefined
+	 */
+	translateX?: 'center';
+
+	/**
+	 * - 세로 축으로 이동하는 정도를 설정합니다.
+	 * @default undefined
+	 */
+	translateY?: 'center';
+
+	/**
+	 * - 컴포넌트의 너비를 설정합니다.
+	 * @default undefined
+	 */
+	width?: CoploreSize;
+
+	/**
+	 * - 컴포넌트의 Z-축 순서를 설정합니다.
+	 * @default undefined
+	 */
+	zIndex?: CoploreZIndex;
 }
