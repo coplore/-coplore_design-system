786469b34189141ec520a4dfbd3861aaeea28d98
chan9yu <wry5533@gmail.com>
2024-02-02 17:46:08 +0900

    feat(@coplore/storybook): flex 스토리 추가 작성

--- apps/coplore-storybook/stories/layouts/Flex.stories.tsx
+++ apps/coplore-storybook/stories/layouts/Flex.stories.tsx
@@ -1,3 +1,4 @@
+import Box from '@coplore/react-ui/src/components/Box';
 import Flex from '@coplore/react-ui/src/components/Flex';
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -12,5 +13,22 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const FlexStory: Story = {
-	args: {}
+	args: {
+		spacing: '25'
+	},
+	render: args => (
+		<>
+			<Flex {...args}>
+				<Box border="sm" borderColor="black-opacity.70" width="20" height="20">
+					item 1
+				</Box>
+				<Box border="sm" borderColor="black-opacity.70" width="20" height="20">
+					item 2
+				</Box>
+				<Box border="sm" borderColor="black-opacity.70" width="20" height="20">
+					item 3
+				</Box>
+			</Flex>
+		</>
+	)
 };
